dd8855d60c5712aa49b0833d2e83d806553319a1
Amirouche <amirouche@hypermove.net>
2018-12-02 16:39:00 +0100

    wip implementation of a meta-command that use the mini-buffer
    Because termbox, M-x doesn't exists as an event. termbox's 'event-key'
    always include a 'control' modifier and alt-fu is considered to be an
    event char except if there is a 'control' modifier like 'C-M-x'. For
    the time being the meta-command is available via 'C-x'. You can quit
    with 'C-q'.
    
    Also:
    
    - rename 'buffer' to 'frame-buffer' to avoid confusion with the
      datastructure that holds the file content defined in zk/buffer.scm
    
    - rename 'vertical' to 'frame-vertical' and 'horizontal' to
      'frame-horizontal' to follow the lead the rename to 'frame-buffer,
      because all those records take part in the layout and now it's clear
      that they are the "same" kind of things.

--- zk.scm
+++ zk.scm
@@ -21,34 +21,34 @@
 ;; model
 
 (define-record-type <model>
-  (make-model root buffers mini-buffer focus)
+  (make-model root frame-buffers mini-buffer focus)
   model?
   (root model-root model-root!)
-  (buffers model-buffers model-buffers!)
+  (frame-buffers model-frame-buffers model-frame-buffers!)
   (mini-buffer model-mini-buffer model-mini-buffer!)
   (focus model-focus model-focus!))
 
 (define-record-type <frame>
-  (make-frame buffer parent position topleft)
+  (make-frame frame-buffer parent position topleft)
   frame?
-  (buffer frame-buffer frame-buffer!)
+  (frame-buffer frame-buffer frame-buffer!)
   (parent frame-parent frame-parent!)
   (position frame-position frame-position!)
   (topleft frame-topleft frame-topleft!))
 
-(define-record-type <vertical>
+(define-record-type <frame-vertical>
   (make-verical parent up down)
-  vertical?
-  (parent vertical-parent vertical-parent!)
-  (up vertical-up vertical-up!)
-  (down vertical-down vertical-down!))
+  frame-vertical?
+  (parent frame-vertical-parent frame-vertical-parent!)
+  (up frame-vertical-up frame-vertical-up!)
+  (down frame-vertical-down frame-vertical-down!))
 
-(define-record-type <horizontal>
-  (make-horizontal parent left right)
-  horizontal?
-  (parent horizontal-parent horizontal-parent!)
-  (left horizontal-left horizontal-left!)
-  (right horizontal-right horizontal-right!))
+(define-record-type <frame-horizontal>
+  (make-frame-horizontal parent left right)
+  frame-horizontal?
+  (parent frame-horizontal-parent frame-horizontal-parent!)
+  (left frame-horizontal-left frame-horizontal-left!)
+  (right frame-horizontal-right frame-horizontal-right!))
 
 (define-record-type <mode>
   (make-mode name bindings)
@@ -56,16 +56,16 @@
   (name mode-name)
   (bindings mode-bindings))
 
-(define-record-type <buffer>
-  (make-buffer path data view mode insert)
-  buffer?
-  (path buffer-path buffer-path!)
-  (data buffer-data buffer-data!)
-  (view buffer-view buffer-view!)
-  (mode buffer-mode buffer-mode!)
-  (insert buffer-insert buffer-insert!))
+(define-record-type <frame-buffer>
+  (make-frame-buffer path data view mode insert)
+  frame-buffer?
+  (path frame-buffer-path frame-buffer-path!)
+  (data frame-buffer-data frame-buffer-data!)
+  (view frame-buffer-view frame-buffer-view!)
+  (mode frame-buffer-mode frame-buffer-mode!)
+  (insert frame-buffer-insert frame-buffer-insert!))
 
-;; buffer
+;;
 
 (define %filename "zk.scm")
 
@@ -98,14 +98,23 @@
         (loop (+ 1 index) (cdr chars))))))
 
 
+(define frame-view (compose frame-buffer-view frame-buffer))
+
 (define (render model)
-  (print 0 "echo"))
+  (if (model-mini-buffer model)
+      (let ((view (frame-view (model-mini-buffer model))))
+        (view model (make-position 0 0) (make-position 0 0)))))
+
+(define (model-current-focus model)
+  (if (model-mini-buffer model)
+      (model-mini-buffer model)
+      (model-focus model)))
 
 (define model-current-bindings
-  (compose mode-bindings buffer-mode frame-buffer model-focus))
+  (compose mode-bindings frame-buffer-mode frame-buffer model-current-focus))
 
 (define model-current-insert
-  (compose buffer-insert frame-buffer model-focus))
+  (compose frame-buffer-insert frame-buffer model-current-focus))
 
 (define (dispatch model)
   (let ((bindings (model-current-bindings model))
@@ -116,17 +125,81 @@
 
 ;; main loop
 
+(define (make-position x y)
+  (cons x y))
+
+(define position-x car)
+(define position-y cdr)
+
+(define (make-bindings)
+  (make-hashtable equal-hash equal?))
+
+(define scheme-bindings (make-bindings))
+
 (define (zk-exit model key)
   (tb-shutdown)
   (exit))
 
-(define (frame-split model key)
-  (let ((clone (frame-clone frame)))
-    #f))
-
-(define scheme-bindings (make-hashtable equal-hash equal?))
 (hashtable-set! scheme-bindings '((#t #f) . q) zk-exit)
-(hashtable-set! scheme-bindings TB-KEY-CTRL-S frame-split)
+
+;; meta command
+
+;; TODO: replace with combinators
+(define (make-binding ctrl alt symbol)
+  (cons (list ctrl alt) symbol))
+
+(define key-char (compose char->integer cdr))
+
+(define (meta-command-view model top-left bottom-right)
+  (print 0 "meta-command-view")
+  (let ((mini-buffer (model-mini-buffer model)))
+    (let ((frame-buffer (frame-buffer mini-buffer)))
+      (let ((buffer (frame-buffer-data frame-buffer)))
+        (print 1 (buffer->string buffer)))
+      (tb-set-cursor (position-x (frame-position mini-buffer))
+                     (position-y (frame-position mini-buffer))))))
+
+(define (meta-command-on-enter model key)
+  (dg 'mini-buffer-input (buffer->string
+                          (frame-buffer-data
+                           (frame-buffer
+                            (model-mini-buffer model)))))
+  (zk-exit model #f))
+
+(define (meta-command-mode)
+  (let ((bindings (make-bindings)))
+    (hashtable-set! bindings (make-binding #f #f 'enter) meta-command-on-enter)
+    (hashtable-set! bindings (make-binding #t #f #\q) zk-exit)
+    (make-mode "meta-command" bindings)))
+
+(define (meta-command-insert model key)
+  ;; meta-command always works with mini-buffer
+  (let* ((frame (model-mini-buffer model))
+         (frame-buffer* (frame-buffer frame))
+         (buffer (frame-buffer-data frame-buffer*)))
+    (let ((new (buffer-char-insert buffer
+                                   0
+                                   (position-y (frame-position frame))
+                                   (key-char key))))
+      (frame-buffer-data! frame-buffer* new)
+      (frame-position! frame (make-position (position-x (frame-position frame))
+                                            (+ 1 (position-y (frame-position frame))))))))
+
+(define (meta-command key)
+  (let ((frame-buffer (make-frame-buffer #f
+                                         (string->buffer "")
+                                         meta-command-view
+                                         (meta-command-mode)
+                                         meta-command-insert)))
+    (make-frame frame-buffer
+                #f
+                (make-position 1 0)
+                (make-position 1 0))))
+
+(define (zk-meta-command model key)
+  (model-mini-buffer! model (meta-command key)))
+
+(hashtable-set! scheme-bindings (make-binding #t #f #\x) zk-meta-command)
 
 (define scheme-mode (make-mode "scheme" scheme-bindings))
 
@@ -142,18 +215,18 @@
 (define (filename->buffer filename)
   (string->buffer (call-with-input-file filename read-string)))
 
-(define buffer (make-buffer filename
+(define %frame-buffer (make-frame-buffer filename
                             (filename->buffer %filename)
                             (lambda (model) (dg 'view))
                             scheme-mode
                             (lambda (model key) (dg 'insert))))
 
-(define root (make-frame buffer '() '(0 . 0) '(0 . 0)))
+(define root (make-frame %frame-buffer '() '(0 . 0) '(0 . 0)))
 
-(define buffers (make-hashtable string-hash string=?))
-(hashtable-set! buffers filename buffer)
+(define frame-buffers (make-hashtable string-hash string=?))
+(hashtable-set! frame-buffers filename %frame-buffer)
 
-(define model (make-model root buffers #f root))
+(define model (make-model root frame-buffers #f root))
 
 (define (main)
   (tb-init)
